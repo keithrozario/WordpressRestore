b338c4011822ac783578852569033b20fdf749b5
Keith Rozario <keithjosephrozario@gmail.com>
2017-01-04 22:57:21 +0800

    Check for existence of WPCONFPASS

--- Backup.sh
+++ Backup.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+WPCONFPASSFILE=~/.wpconfpass
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    --wpconfpass)
+    WPCONFPASSARG="$2"
+    shift # past argument
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ -f $WPCONFPASSFILE ]; then
+  if [ -z '$WPCONFPASSARG' ]; then #File exist, Arg not supplied
+    $(cat $WPCONFPASSFILE | grep -v ^# | xargs)
+  else #File exist, Arg supplied
+    echo "Encryption key provided, but one already exist, over-riding existing key"
+    $(cat $WPCONFPASSFILE | grep -v ^# | xargs)
+    rm $WPCONFPASSFILE
+    echo "WPCONFPASS=$WPCONFPASSARG" > $WPCONFPASSFILE
+  fi
+else
+   if [ -z '$WPCONFPASSARG' ]; then #File does not exist, Arg not supplied
+    echo "Encryption does not exist on system, and is not provided, aborting"
+    exit 0
+   else #File does not exist, Arg supplied
+    echo "First Time setup, saving encryption key"
+    $(cat $WPCONFPASSFILE | grep -v ^# | xargs)
+    rm $WPCONFPASSFILE
+    echo "WPCONFPASS=$WPCONFPASSARG" > $WPCONFPASSFILE
+   fi
+fi
+
 
 ##Filenames#########################
 WPSQLFILE=wordpress.sql
