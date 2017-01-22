078cca3d65394e2a1b7545cdea00b4011487caaf
Keith Rozario <keithjosephrozario@gmail.com>
2017-01-23 00:00:12 +0800

    Version 1.0
    First Version

--- backupWP.sh
+++ backupWP.sh
@@ -12,6 +12,8 @@
 # http://creativecommons.org/licenses/by/4.0/ or 
 # send a letter to 
 # Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+#
+
 
 WPSETTINGSFILE=/var/.wpsettings
 ENCKEYFILE=/var/.enckey
